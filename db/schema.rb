5017663af5f0f2d2d6a2e0973b34ab5149e5731b
Jason Willems <jason@willems.ca>
2018-03-19 00:38:46 -0700

    Add cancelled column to conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109054559) do
+ActiveRecord::Schema.define(version: 20180315154753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180109054559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phonenumber"
+    t.boolean "cancelled", default: false
   end
 
   create_table "users", force: :cascade do |t|
